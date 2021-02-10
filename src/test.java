4b3c63e0658f288bd99152be48e9e125794c9b02
LevSavinkov <www.savinkov@gmail.com>
2021-02-10 20:34:36 +0400

    Чет сделал

---
+++ src/test.java
@@ -0,0 +1,28 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 3;
+        if (a==5) {
+            System.out.println("Ответ 5 ");
+        }
+        if (a==6) {
+            System.out.println("Ответ 6 ");
+        }
+        if (a==7) {
+            System.out.println("Ответ 7 ");
+        }
+        if (a!=5 || a!=6 || a!=7) {
+            System.out.println("Не то вводишь, Алеша! ");
+        }
+/*        switch (a) {
+            case 5 :
+                System.out.println("Ответ 5 ");
+                break;
+            case 6 :
+                System.out.println("Ответ 6 ");
+                break;
+            case 7 :
+                System.out.println("Ответ 7 ");
+                break;
+        }*/
+    }
+}
